32a64de27ab074485fe2b9f7346534b288454f40
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-27 16:19:26 +0200

    changed the errors display method

--- controller/inscription.php
+++ controller/inscription.php
@@ -3,24 +3,23 @@ require_once '../model/data.php';
 require_once '../vue/template/head.php';
 require 'headerc.php';
 
-$error = [];
+$errors = [];
 
 if (isset($_POST['user'], $_POST['userfname'], $_POST['userlname'], $_POST['email'], $_POST['pwd'], $_POST['rpwd'])
   && !empty($_POST['user']) && !empty($_POST['userfname']) && !empty($_POST['userlname']) && !empty($_POST['email'])
   && !empty($_POST['pwd']) && !empty($_POST['rpwd'])) {
-
-    if(!preg_match('#^[a-z0-9]+$#', $_POST['user'])) {
-      $error[] = "Le nom d'utilisateur doit comporter uniquement des lettres minuscules ou des chiffres.";
+    if (!preg_match('#^[a-z0-9]+$#', $_POST['user'])) {
+        $errors[] = "Le nom d'utilisateur doit comporter uniquement des lettres minuscules ou des chiffres.";
     }
-    if(!preg_match('#^[A-Z][A-Za-z-\s]*[a-z]$#', $_POST['userlname']) || !preg_match('#^[A-Z][a-z-\s]+[a-z]$#', $_POST['userfname'])) {
-      $error[] = 'Le nom et le prénom ne peuvent contenir que des lettres et commencer par une majuscule.';
+    if (!preg_match('#^[A-Z][A-Za-z-\s]*[a-z]$#', $_POST['userlname']) || !preg_match('#^[A-Z][a-z-\s]+[a-z]$#', $_POST['userfname'])) {
+        $errors[] = 'Le nom et le prénom ne peuvent contenir que des lettres et commencer par une majuscule.';
     }
-    if($_POST['pwd'] != $_POST['rpwd'] && !preg_match('#^[a-z0-9]+$#', $_POST['pwd']) && !preg_match('#^[a-z0-9]+$#', $_POST['rpwd'])) {
-      $error[] = 'Les mots de passe doivent être identiques et ne comporter que des lettres minuscules ou des chiffres';
+    if ($_POST['pwd'] != $_POST['rpwd'] || !preg_match('#^[a-z0-9]+$#', $_POST['pwd']) || !preg_match('#^[a-z0-9]+$#', $_POST['rpwd'])) {
+        $errors[] = 'Les mots de passe doivent être identiques et ne comporter que des lettres minuscules ou des chiffres';
     }
 
 
-    if (count($error) == 0) {
+    if (count($errors) == 0) {
         $user_creation[] = $_POST['user'];
         $user_creation[] = $_POST['userfname'];
         $user_creation[] = $_POST['userlname'];
@@ -31,18 +30,18 @@ if (isset($_POST['user'], $_POST['userfname'], $_POST['userlname'], $_POST['emai
         $user_creation = sanitize($user_creation);
 
         if (verify_user($user_creation[0])) {
-          $error[] = verify_user($user_creation[0]);
+            $errors[] = verify_user($user_creation[0]);
+        } else {
+            add_user($user_creation);
         }
-        else {
-          add_user($user_creation);
-        }
-
     }
 }
-if ($error) {
-    print_r($error);
-} else {
-    echo "pas d'erreurs";
+if ($errors) {
+    foreach ($errors as $error) {
+      ?>
+      <p><?php echo $error; ?></p>
+      <?php
+    }
 }
 
 include '../vue/inscriptionv.php';
--- model/adduser.php
+++ model/adduser.php
@@ -23,19 +23,22 @@ function add_user($user_creation)
 
         $dbconnect->beginTransaction();
 
+        $user_creation[4] = password_hash($user_creation[4], PASSWORD_BCRYPT);
+
         $req_v = $dbconnect->prepare('INSERT INTO users(user_name, user_password, user_first_name, user_last_name, user_email)
                     VALUES(:user_name, :user_pwd, :user_fname, :user_lname, :user_email)');
-        $req_v->execute(array('user_name'->$user_creation[0],
-                            'user_pwd'->password_hash($user_creation[4], PASSWORD_BCRYPT),
-                            'user_fname'->$user_creation[1],
-                            'user_lname'->$user_creation[2],
-                            'user_email'->$user_creation[3]));
+        $req_v->execute(array('user_name'=>$user_creation[0],
+                            'user_pwd'=>$user_creation[4],
+                            'user_fname'=>$user_creation[1],
+                            'user_lname'=>$user_creation[2],
+                            'user_email'=>$user_creation[3]));
 
         $dbconnect->commit();
     } catch (Exception $e) {
         $dbconnect->rollBack();
 
         $errors = $e->getMessage();
+        return $errors;
     }
-    // return $errors;
+    return "Utilisateur créé";
 }
